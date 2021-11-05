7a247342dfbb5b5820b706f45fb2063602f20489
Avi Romanoff <aroman@users.noreply.github.com>
2021-11-04 21:23:52 -0700

    Update ReproductionTest.cs

--- Assets/ReproductionTest.cs
+++ Assets/ReproductionTest.cs
@@ -8,6 +8,8 @@ public class ReproductionTest : MonoBehaviour
 {
     private void Start()
     {
+        // Each one of these lines is required for the crash to occur. (The debug statements don't matter of course).
+        // You can try removing one and confirming the segfault does not occur.
         MessageReceivedEventArgs foo;
         MobileAds.Initialize(_ => Debug.Log("Admob init"));
         FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(_ => Debug.Log("firebase init"));
